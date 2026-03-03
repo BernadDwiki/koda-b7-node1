071944d4795b934ab459193907cacb2743115f64
BernadDwiki <125329942+BernadDwiki@users.noreply.github.com>
2026-03-03 16:29:26 +0700

    Update minitask-2.js

--- minitask-2.js
+++ minitask-2.js
@@ -1,12 +1,12 @@
-let regexStrip = /^\d{2}-\d{2}-\d{4}$/;
+const regexStrip = /^\d{2}-\d{2}-\d{4}$/;
 
 function dateConversion(tanggal){
     if(regexStrip.test(tanggal)){
-        let [day, month, year] = tanggal.split('-');
-        let formattedDate = `${day}/${month}/${year}`;
+        const [day, month, year] = tanggal.split('-');
+        const formattedDate = `${day}/${month}/${year}`;
         return formattedDate;
     } else{
-        return "Format Tanggal Salah"
+        return 'Format Tanggal Salah'
     }
 }
 
