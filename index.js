141bbc4265fe2a4356887dab9a84a9a3e8ff0acf
BernadDwiki <125329942+BernadDwiki@users.noreply.github.com>
2026-03-03 13:00:16 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const dateConversion = require("./date.js")
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const askQuestion = (query) => new Promise((resolve) => rl.question(query, resolve));
+
+async function main() {
+  const tanggal = await askQuestion('Masukkan tanggal : ');
+  console.log(dateConversion(tanggal))
+  rl.close();
+}
+
+main();
